e4d0756b4dcfff36eab10742dea5a3bd375297e4
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-13 22:15:56 +0700

    Integrasi dengan HTML

---
+++ html.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Hello PHP";
+$body = "Hello HTML";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<h1><?php echo $body; ?></h1>
+</body>
+</html>
