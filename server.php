96791dbe964ae072270beefef1c86e66d3bc26fd
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-13 22:44:33 +0700

    Global Variable SERVER

---
+++ server.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>$_SERVER</h1>
+
+<table border="1">
+    <?php foreach ($_SERVER as $key => $value): ?>
+    <tr>
+        <td><?= $key ?></td>
+        <td><?= $value ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
