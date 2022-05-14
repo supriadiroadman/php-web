feb0c20de8ec06cb229e2a5439ed3f7fc413c935
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-14 23:33:27 +0700

    Move Uploaded File

--- uploads/upload.php
+++ uploads/upload.php
@@ -6,6 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $file_size = $_FILES['upload_file']['size'];
     $file_tmp_name = $_FILES['upload_file']['tmp_name'];
     $file_error = $_FILES['upload_file']['error'];
+
+   // Buat folder bernama "files" di __DIR__(directory saat ini) yaitu di folder uploads
+    move_uploaded_file($file_tmp_name, __DIR__.'/files/'.$file_name);
 }
 
 ?>
