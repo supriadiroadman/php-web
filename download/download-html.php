d6b72d8b0fb8aecf344b55607f432e96960b812c
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-14 23:50:01 +0700

    Download html dengan fungsi header

---
+++ download/download-html.php
@@ -0,0 +1,13 @@
+<?php
+// Akan mendownload isi html dibawah ini dengan nama downloaded.html
+header('Content-Disposition: attachment; filename="downloaded.html"');
+?>
+<!--Semua dibawah ini akan didownload-->
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h1>Download HTML</h1>
+</body>
+</html>
